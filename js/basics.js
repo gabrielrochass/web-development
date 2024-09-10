a88788ca7ee98285ff71ab82208a8a2694c4c21d
gabrielrochass <120466775+gabrielrochass@users.noreply.github.com>
2024-09-10 12:00:17 -0300

    feat: operadores em js

--- js/basics.js
+++ js/basics.js
@@ -31,11 +31,38 @@ console.log(simbolo1) // symbol('id')
 console.log(simbolo1 == simbolo2) // false
 console.log(typeof bigNumber) // bigInt
 
-
 // var pode ser redeclarado no mesmo escopo, let não!
 // preferencialmente, usar let!!
 
 // operadores: aritmético, comparativo e lógico
+// aritmético
+let number = 1;
+number++; // 2
+number += 3; // 5
+number -= 1; // 4
+number = number % 3; // 1
+number *= 10; // 10
+number /= 2; // 5
+
+// comparativo
+console.log(5 == "5"); // true -> == compara valor, n tipo
+console.log(5 === "5"); // false -> === compara estritivamente tudo
+console.log(5 != 5); // false
+console.log(5 > 9); // false
+console.log(6 < 9); // true
+
+// lógico -> mostrar tabela verdade
+console.log((5 > 2 && 4 < 7)) // true TT  
+console.log((5 > 2 && 4 > 7)) // false TF / FT
+console.log((5 < 2 && 4 > 7)) // fase FF 
+
+console.log((5 > 2 || 4 < 7)) // true TT
+console.log((5 < 2 || 4 < 7)) // true FT TF
+console.log((5 < 2 || 4 > 7)) // false FF
+
+let isTrue = !(5 < 2) // true
+console.log(isTrue)
+
 
 // estruturas de controle: if, else e switch
 
